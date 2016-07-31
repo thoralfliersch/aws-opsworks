f50a4787fdf2e524121023d402a0ed9b509c8470
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-07-31 12:04:41 +0200

    remove all depends from metafile

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,3 @@
 name    "liersch"
 license "proprietary"
 version "0.0.1"
-
-depends "apache"
-depends "apt"
-depends "build-essential"
-depends "git"
-depends "htop"
-depends "php"
-depends 'mysql', '= 7.2.0'
-depends "php-fpm"
