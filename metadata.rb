a134b50feebb63ea84d892722d073203e4eef104
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-08-01 00:37:57 +0200

    moved depend iptables on first position

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name    "liersch"
 license "proprietary"
 version "0.0.1"
 
+depends "iptables", "= 1.0.0"
 depends "apt"
 depends "build-essential"
 depends "nginx", "= 2.7.6"
-depends "iptables", "= 1.0.0"
