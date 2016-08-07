1c13cc96eeb1103e4da1d31effacc49888f9e5ed
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-08-07 20:50:10 +0200

    testing

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,13 +1,7 @@
-#opsworks_deploy_dir do
-    #user deploy[:user]
-    #group deploy[:group]
-    #path "/var/www/vhosts"
-    #path deploy[:deploy_to]
-#end
-
-directory '/var/www/vhosts' do
-    owner "www-data"
-    group "www-data"
-    mode '0755'
-    action :create
+node[:deploy].each do |app, deploy|
+    opsworks_deploy_dir do
+        user deploy[:user]
+        group deploy[:group]
+        path deploy[:deploy_to]
+    end
 end
