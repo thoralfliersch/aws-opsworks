a80ba6c0a84c0f819d0e022fd98921827bed318b
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-08-07 19:22:51 +0200

    correct the directory method

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,6 +1,13 @@
-opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path "/var/www/vhosts"
+#opsworks_deploy_dir do
+    #user deploy[:user]
+    #group deploy[:group]
+    #path "/var/www/vhosts"
     #path deploy[:deploy_to]
+#end
+
+directory '/var/www/vhosts' do
+  owner deploy[:user]
+  group deploy[:group]
+  mode '0755'
+  action :create
 end
