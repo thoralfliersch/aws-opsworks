a575d370d457e3e1ac11b52519ab70f723552e8a
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-08-07 18:06:01 +0200

    added nginx config template

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -6,3 +6,12 @@
 package "nginx" do
     action :upgrade
 end
+
+template 'nginx.conf' do
+    path     "/etc/nginx/nginx.conf"
+    source   "nginx/nginx.conf.erb"
+    owner    'root'
+    group    'root'
+    mode     '0644'
+    notifies :reload, 'service[nginx]', :delayed
+end
